8d50bac0927c813104726e2775edc38f507143e8
H0a1r1u5 <chingya.hu.tw@gmail.com>
2025-07-21 13:25:48 +1200

    add Emily's background

--- project/stone.gd
+++ project/stone.gd
@@ -10,7 +10,3 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	enter = false
 	
-func _process(delta):
-	if entered == true:
-		if  input.is_action_just_pressed("ui_accept")
-			get_tree().chnage_scene("the scene:res:// paste here")
