0fa32b0db3587fab5eacb4b822dee5104176d780
H0a1r1u5 <chingya.hu.tw@gmail.com>
2025-10-03 22:58:50 +1300

    fix sprite 2d not assign problem!

--- project/H2AStone.gd
+++ project/H2AStone.gd
@@ -2,8 +2,8 @@
 extends Area2D
 class_name H2AStone
 
-# Make sure you have a Sprite2D child node
-@onready var sprite: Sprite2D = $Sprite2D
+# Sprite2D will be created automatically
+var sprite: Sprite2D
 
 # Internal storage for slots
 var _target_slot_internal: int = 0
@@ -22,21 +22,29 @@ var current_slot: int:
 	get:
 		return _current_slot_internal
 
-# --- Setter functions ---
+# --- Godot ready function ---
+func _ready():
+	if sprite == null:
+		sprite = Sprite2D.new()
+		add_child(sprite)
+	_update_texture()  # safe now
+
 func _set_target_slot(v: int):
 	_target_slot_internal = v
-	_update_texture()
+	if sprite != null:
+		_update_texture()
 
 func _set_current_slot(v: int):
 	_current_slot_internal = v
-	_update_texture()
+	if sprite != null:
+		_update_texture()
 
-# --- Update the sprite texture ---
+
 func _update_texture():
+	if sprite == null:
+		return  # Do nothing if sprite hasn't been created yet
+
 	var index := _target_slot_internal
 	if _target_slot_internal != _current_slot_internal:
 		index += H2AConfig.Slot.size() - 1
 	sprite.texture = load("res://arts/flags/chess_%02d.webp" % index)
-
- 
-	
