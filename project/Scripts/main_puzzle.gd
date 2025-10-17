9fa3e8f17677a69ddbb2566aea2d4677165114a7
zeyi17 <dongzeyi0626@gmail.com>
2025-10-17 18:27:03 +1300

    1

--- project/Scripts/main_puzzle.gd
+++ project/Scripts/main_puzzle.gd
@@ -1,9 +1,9 @@
 extends Node2D
-@onready var  arr = [$piece/TextureRect, $piece/TextureRect2, $piece/TextureRect3, $piece/TextureRect4, $piece/TextureRect5, $piece/TextureRect6, $piece/TextureRect7, $piece/TextureRect8, $piece/TextureRect9, $piece/TextureRect10, $piece/TextureRect11, $piece/TextureRect12]
+@onready var  arr = [$cell/TextureRect, $cell/TextureRect2, $cell/TextureRect3, $cell/TextureRect4, $cell/TextureRect5, $cell/TextureRect6, $cell/TextureRect7, $cell/TextureRect8, $cell/TextureRect9, $cell/TextureRect10, $cell/TextureRect11, $cell/TextureRect12]
 # @export var list : Array[Texture]
 func _ready() -> void:
 	for node in arr:
-		node.set_meta("Whether occupied",false)
+		node.set_meta("WhetherOccupied",false)
 	for node: TextureRect in $piece .get_children():
 		node.gui_input.connect(func(event: InputEvent):
 			if(event is InputEventScreenDrag):
@@ -11,18 +11,18 @@ func _ready() -> void:
 			if event is InputEventScreenTouch:
 				if !event.pressed:
 					for cell in arr:
-						print(cell.get_meta("Whether occupied"))
-						#print(cell.global_position.distance_to(node.global_position))
-						if cell.global_position.distance_to(node.global_position) < 300 and !cell.get_meta("Whether occupied", false):
-							cell.set_meta("Whether occupied", true)
+						print(cell.get_meta("WhetherOccupied"))
+						print(cell.global_position.distance_to(node.global_position))
+						if cell.global_position.distance_to(node.global_position) < 300 and !cell.get_meta("WhetherOccupied", false):
+							cell.set_meta("WhetherOccupied", true)
 							node.global_position = cell.global_position
 						pass
 					pass
 				else:
 					for cell in arr:
 						#print(cell.global_position.distance_to(node.global_position))
-						if cell.global_position.distance_to(node.global_position) < 100 and cell.get_meta("Whether occupied", false):
-							cell.set_meta("Whether occupied", false)
+						if cell.global_position.distance_to(node.global_position) < 100 and cell.get_meta("WhetherOccupied", false):
+							cell.set_meta("WhetherOccupied", false)
 						
 					pass
 				pass
