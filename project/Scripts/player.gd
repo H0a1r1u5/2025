5da601afd21618e6e5a238f15067ce95269e262c
zeyi17 <dongzeyi0626@gmail.com>
2025-10-17 17:52:42 +1300

    1

--- project/Scripts/player.gd
+++ project/Scripts/player.gd
@@ -12,6 +12,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		pause_screen.show_pause()
 
 func _physics_process(delta: float) -> void:
+	# Get the player input direction vector
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
 	if direction:
@@ -20,13 +21,22 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		
-
+#Play moving or idle animation
 	if velocity != Vector2.ZERO:
 		animated_sprite_2d.play_movement_animation(velocity)
 	else:
 		animated_sprite_2d.play_idle_animation()
-	
+	# Move Player
 	move_and_slide()
+
+	var min_x = 0
+	var max_x = 1152
+	var min_y = 0
+	var max_y = 648
+
+# Limit the player to the screen boundaries
+	position.x = clamp(position.x, min_x, max_x)
+	position.y = clamp(position.y, min_y, max_y)
 
 
 func _on_interactable_body_entered(body: Node2D) -> void:
